f5aebb0d57bbcc0ebae3e8c25bd8e218e810f10e
«AlexeyGoroshchenya» <375295509897@yandex.ru>
2022-05-05 11:51:13 +0300

    законичил адаптив

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -3,41 +3,59 @@ import { animate } from "./helpers";
 export const scroll = () => {
 
     const btnScrollUp = document.querySelector('.arrow')
+    const borderBtnLine = (document.querySelector('.popular').offsetTop);
 
-    const goTo = (target, direction) => {
+    const moveBtn = (str) => {
+        if (str === 'hide') {
+            btnScrollUp.classList.remove('arrow-active');
+        } else if (str === 'show') {
+            btnScrollUp.classList.add('arrow-active');
+        }
+    }
+
+    const goTo = (target) => {
 
         const linkTargetName = target.getAttribute('href').replace('#', '')
+        const linkTarget = document.getElementById(linkTargetName)
 
-        animate({
-            duration: 300,
-            timing(timeFraction) {
-                return timeFraction;
-            },
-            draw(progress) {
+        if (linkTarget) {
+            animate({
+                duration: 300,
+                timing(timeFraction) {
+                    return timeFraction;
+                },
+                draw(progress) {
 
-                let count = 0;
-                if (direction === 'top') {
-                    count = target.offsetTop - (target.offsetTop * progress)
-                } else { count = document.getElementById(linkTargetName).offsetTop * progress }
+                    let count = target.offsetTop - (target.offsetTop - linkTarget.offsetTop) * progress
 
-
-                window.scrollTo({
-                    top: count,
-                })
-            }
-        });
-
-
+                    window.scrollTo({
+                        top: count,
+                    })
+                }
+            });
+        }
 
     }
 
     document.body.addEventListener('click', (e) => {
-
-
         if (e.target.closest('.arrow')) {
             e.preventDefault()
             goTo(btnScrollUp, 'top')
         }
 
+        if (e.target.classList.contains('menu-header__link') || e.target.matches('.footer__links>a')) {
+            e.preventDefault()
+            goTo(e.target)
+        }
+
     })
+
+    window.addEventListener('scroll', function () {
+
+        if (scrollY >= borderBtnLine) {
+            moveBtn('show')
+        } else {
+            moveBtn('hide')
+        }
+    });
 }
